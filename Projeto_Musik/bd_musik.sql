4cf7fb09ed77247615d6f73522db126bf1521feb
Gabriel Mendes Boaventura <gabriel.boaventura@dinfor.uniceub>
2025-05-19 10:47:44 -0300

    Subindo o banco de dados

--- Projeto_Musik/bd_musik.sql
+++ Projeto_Musik/bd_musik.sql
@@ -1,38 +1,92 @@
-create
-database SiteMusica;
-
-use
-SiteMusica;
-create table `sitemusica`.`Usuario`
-(
-    `id`           int          not null auto_increment,
-    `nome`         varchar(100) not null,
-    `senha`        varchar(45)  not null,
-    `email`        varchar(45)  not null,
-    `genero_music` varchar(100) not null,
-    primary key (`id`)
-);
-
-insert into Usuario (nome, senha, email, genero_music)
-values ('Gabriel', 123456, 'gabriel.boaventura@sempreceub.com', 'Rock, MPB, Choro');
-insert into Usuario (nome, senha, email, genero_music)
-values ('Cecília', 123456, 'cecilia.formiga@sempreceub.com', 'Hinos, MPB, Erucito, Rock');
+DROP DATABASE sitemusica;
+CREATE SCHEMA IF NOT EXISTS `sitemusica` DEFAULT CHARACTER SET utf8 ;
+USE `sitemusica`;
+-- -----------------------------------------------------
+-- Table `sitemusica`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sitemusica`.`Usuario` (
+                                                      `idUsuario` INT NOT NULL AUTO_INCREMENT,
+                                                      `nome` VARCHAR(45) NULL,
+    `email` VARCHAR(100) NULL,
+    `biografia` TEXT NULL,
+    `senha` VARCHAR(50) NULL,
+    `minha_music` VARCHAR(45) NULL,
+    `data_ini` DATE NULL,
+    `data_nasc` DATE NULL,
+    PRIMARY KEY (`idUsuario`))
+    ENGINE = InnoDB;
 
 
-Select *
-from Usuario
---Implementando a tabela amigos
-CREATE TABLE amigos
-(
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    nome            VARCHAR(100) NOT NULL,
-    genero_favorito VARCHAR(100),
-    foto_url        VARCHAR(255)
-);
-
-INSERT INTO amigos (nome, genero_favorito, foto_url)
-VALUES ('Ana Souza', 'MPB e Indie', 'https://via.placeholder.com/60'),
-       ('Lucas Oliveira', 'Rock Clássico', 'https://via.placeholder.com/60'),
-       ('Mariana Lima', 'Lo-fi e Jazz', 'https://via.placeholder.com/60');
+-- -----------------------------------------------------
+-- Table `sitemusica`.`Musica`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sitemusica`.`Musica` (
+                                                     `idMusica` INT NOT NULL AUTO_INCREMENT,
+                                                     `genero` VARCHAR(45) NULL,
+    `data` DATE NULL,
+    `artista` VARCHAR(45) NULL,
+    PRIMARY KEY (`idMusica`))
+    ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `sitemusica`.`composicao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sitemusica`.`composicao` (
+                                                         `idcomposicao` INT NOT NULL AUTO_INCREMENT,
+                                                         `titulo` VARCHAR(45) NULL,
+    `composicaocol` VARCHAR(45) NULL,
+    `Usuario_idUsuario` INT NOT NULL,
+    `Documento` blob,
+    PRIMARY KEY (`idcomposicao`),
+    INDEX `fk_composicao_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
+    CONSTRAINT `fk_composicao_Usuario1`
+    FOREIGN KEY (`Usuario_idUsuario`)
+    REFERENCES `sitemusica`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sitemusica`.`Usuario_das_Musica`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sitemusica`.`Usuario_das_Musica` (
+                                                                 `Usuario_idUsuario` INT NOT NULL,
+                                                                 `Musica_idMusica` INT NOT NULL,
+                                                                 PRIMARY KEY (`Usuario_idUsuario`, `Musica_idMusica`),
+    INDEX `fk_Usuario_has_Musica_Musica1_idx` (`Musica_idMusica` ASC) VISIBLE,
+    INDEX `fk_Usuario_has_Musica_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
+    CONSTRAINT `fk_Usuario_has_Musica_Usuario1`
+    FOREIGN KEY (`Usuario_idUsuario`)
+    REFERENCES `sitemusica`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_Usuario_has_Musica_Musica1`
+    FOREIGN KEY (`Musica_idMusica`)
+    REFERENCES `sitemusica`.`Musica` (`idMusica`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `sitemusica`.`amigos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `sitemusica`.`amigos` (
+                                                     `idUsuario` INT NOT NULL,
+                                                     `idAmigo` INT NOT NULL,
+                                                     PRIMARY KEY (`idUsuario`, `idAmigo`),
+    INDEX `fk_Usuario_has_Usuario_Usuario2_idx` (`idAmigo` ASC) VISIBLE,
+    INDEX `fk_Usuario_has_Usuario_Usuario1_idx` (`idUsuario` ASC) VISIBLE,
+    CONSTRAINT `fk_Usuario_has_Usuario_Usuario1`
+    FOREIGN KEY (`idUsuario`)
+    REFERENCES `sitemusica`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_Usuario_has_Usuario_Usuario2`
+    FOREIGN KEY (`idAmigo`)
+    REFERENCES `sitemusica`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
